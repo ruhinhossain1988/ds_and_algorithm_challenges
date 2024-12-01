a87444ccbc3fcac35556e40154c2b34398f27112
Ruhin <ruhinhossain88@gmail.com>
2024-12-01 23:17:36 +0600

    towerOfHanoi solution 2 is added

--- 50DaysDSAndAlgorithm/recursion/day3/TowerOfHanoi.js
+++ 50DaysDSAndAlgorithm/recursion/day3/TowerOfHanoi.js
@@ -9,4 +9,30 @@ let towerOfHanoi = function (n, source, aux, destination) {
     towerOfHanoi(n - 1, aux, source, destination);
 };
 
+function toh(N, fromm, to, aux) {
+    //Write code here
+    // sample print statement below
+    //console.log("move disk " + 1 + " from rod " + 1 + " to rod " + 2);
+    //in the above statement consider we are moving disk 1 from rod 1 to rod 2
+    //remember to return number of moves as well
+    let count = 0;
+    function helper(N, fromm, to, aux){
+
+        if (N === 1) {
+            count+=1;
+            console.log("move disk " + N + " from rod " + fromm + " to rod " + to);
+            return;
+        }
+
+        helper(N - 1, fromm, to, aux);
+        count+=1;
+        console.log("move disk " + N + " from rod " + fromm + " to rod " + to);
+        helper(N - 1, aux, fromm, to);
+    }
+    helper(N, fromm, to, aux)
+    return count;
+}
+
+
 towerOfHanoi(3, 'A', 'B', 'C');
+toh(3, 1, 3, 2);
